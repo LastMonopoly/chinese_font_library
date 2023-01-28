b6d11ed8b7d01ffdb690ef1b3ba76c8a34a96f22
Terminator <lastmonopoly@outlook.com>
2023-01-28 22:49:15 +0800

    Add font weight related functions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,139 @@
+import 'package:chinese_font_library/chinese_font_library.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const App());
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _AppState extends State<App> {
+  bool useDefaultChineseFont = true;
+  int currentPageIndex = 0;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Theme Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        textTheme: useDefaultChineseFont ? DefaultChineseFont.textTheme : null,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Chinese Font Library Demo'),
+          actions: [
+            Switch(
+                value: useDefaultChineseFont,
+                onChanged: (value) {
+                  setState(() {
+                    useDefaultChineseFont = value;
+                  });
+                })
+          ],
+          centerTitle: false,
+        ),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.text_fields),
+              label: 'Font weight',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            NavigationDestination(
+              icon: Icon(Icons.palette_outlined),
+              selectedIcon: Icon(Icons.palette),
+              label: 'Text theme',
             ),
           ],
         ),
+        body: currentPageIndex == 0
+            ? const FontWeightDemo()
+            : const TextThemeDemo(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class FontWeightDemo extends StatelessWidget {
+  const FontWeightDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        _buildFittedText(FontWeight.w100),
+        _buildFittedText(FontWeight.w200),
+        _buildFittedText(FontWeight.w300),
+        _buildFittedText(FontWeight.w400),
+        _buildFittedText(FontWeight.w500),
+        _buildFittedText(FontWeight.w600),
+        _buildFittedText(FontWeight.w700),
+        _buildFittedText(FontWeight.w800),
+        _buildFittedText(FontWeight.w900),
+      ],
+    );
+  }
+
+  _buildFittedText(FontWeight weight) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: FittedBox(
+        child: Text(
+          '焦虑时代 一方净土 Hello world',
+          style: TextStyle(fontWeight: weight),
+        ),
+      ),
+    );
+  }
+}
+
+class TextThemeDemo extends StatelessWidget {
+  const TextThemeDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: _buildThemedText(context));
+  }
+
+  List<Widget> _buildThemedText(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final textList = <Widget>[];
+
+    final styles = <TextStyle?>[
+      textTheme.displayLarge,
+      textTheme.displayMedium,
+      textTheme.displaySmall,
+      textTheme.headlineLarge,
+      textTheme.headlineMedium,
+      textTheme.headlineSmall,
+      textTheme.titleLarge,
+      textTheme.titleMedium,
+      textTheme.titleSmall,
+      textTheme.bodyLarge,
+      textTheme.bodyMedium,
+      textTheme.bodySmall,
+      textTheme.labelLarge,
+      textTheme.labelMedium,
+      textTheme.labelSmall,
+    ];
+    for (var style in styles) {
+      textList.add(Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          '给心灵放个假',
+          style: style?.copyWith(fontWeight: FontWeight.w200),
+        ),
+      ));
+    }
+    return textList;
   }
 }
