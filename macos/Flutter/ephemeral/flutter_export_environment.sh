b0ab6d79bdabd002513fd14cc3eb894ea4513371
Xhan <LastMonopoly@users.noreply.github.com>
2025-12-13 01:07:08 +0800

    Update dependencies and Gradle configuration; migrate to Flutter 3.24

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/terminator/fvm/versions/stable"
-export "FLUTTER_APPLICATION_PATH=/Users/terminator/Developer/chinese_font_library"
+export "FLUTTER_ROOT=/Users/xhan/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/xhan/Developer/Github/chinese_font_library"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.2.0"
